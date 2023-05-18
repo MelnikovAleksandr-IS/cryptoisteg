dceaf3382ecf91b20860e17fe8203433717a5ac3
MelnikovAleksandr-IS <115283895+MelnikovAleksandr-IS@users.noreply.github.com>
2023-05-18 20:30:10 +0300

    Update shHilla.py

--- lab_2/shHilla.py
+++ lab_2/shHilla.py
@@ -1,34 +1,46 @@
 import numpy as np
 
-'''
-Зашифровка шифра Хилла (1)
-'''
-key = np.array([1, 3, 5], [2, 4, 6], [7, 8, 9]) #Создание матрицы ключа
 
-message = "CRYPTOGRAPHY"
-blsize = len(key)                                                    #Разбиение сообщения на блоки
-bls =  [message[i: i+blsize] for i in range(0, len(message), blsize)]
+message1 = "CRYPTOGRAPHY"
+key1 = np.array([[0, 12, 29], [16, 9, 14], [9, 8, 13]]) #Создание ключа-матрицы
 
-nbl = [[ord(j) - 65 for j in c] for c in bls] #Преобразуем буквенных блоков в блоки чисел
+cipbls1 = []
+key_inv1 = np.linalg.inv(key1) #Обратная ключ-матрица
+bls = []
 
-cipn = [np.dot(key, c) % 26 for c in nbl] #Умножаем ключ-матрицу на блок чисел
+def shhill(message, key):
+  global cipbls1, bls
+  '''
+  Зашифровка шифра Хилла (1)
+  '''
+  blsize = len(key)                                                    #Разбиение сообщения на блоки
+  bls =  [message[i: i+blsize] for i in range(0, len(message), blsize)]
+  
+  nbl = [[ord(j) - 65 for j in c] for c in bls] #Преобразуем буквенных блоков в блоки чисел
+  
+  cipn = [np.dot(key, c) % 26 for c in nbl] #Умножаем ключ-матрицу на блок чисел
+  
+  cipbls1 = [''.join([chr(n + 65) for n in c]) for c in cipn] #Преобразование чисел в буквы
+  cip = ''.join(cipbls1)
+  
+  return cip
 
-cipbls = ''.join([chr(n + 65) for n in c]) for c in cipn] #Преобразование чисел в буквы
-cip = ''.join(cipbls)
+print(shhill(message1, key1))
 
-print(cip)
 
-'''
-Расшифровка (1)
-'''
-key_inv = np.linalg.inv(key) #Обратная ключ-матрица
+def decshhilla(cipbls, key_inv):
+  global bls, cipbls1
+  '''
+  Расшифровка (1)
+  '''
+  cipn = [[ord(j) - 65 for j in c] for c in bls] #Преобразование зашифрованных блоков в числа
+  
+  decn = [np.dot(key_inv, c) % 26 for c in cipn] #Умножение обратной матрицы ключа на блок чисел
+  
+  decbls = [''.join([chr(n + 65) for n in c]) for c in decn] #Преобразование расшифрованных чисел обратно в буквы
+  dec = ''.join(decbls)
+  
+  return dec
 
-cipbls = ['EDC', 'RFT', 'OFW']
-cipn = [[ord(j) - 65 for j in c] for c in cipbls] #Преобразование зашифрованных блоков в числа
-
-decn = [np.dot(key_inv, c) % 26 for c in cipn] #Умножение обратной матрицы ключа на блок чисел
-
-decbls = [''.join([chr(n + 65) for n in c]) for c in decn] #Преобразование расшифрованных чисел обратно в буквы
-dec = ''.join(decbls)
-
-print(dec) 
+print(decshhilla(cipbls1, key_inv1))
+  
