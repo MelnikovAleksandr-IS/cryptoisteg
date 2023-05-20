d8fbfc356063f8c01bf1cb2e3769234785f44d64
MelnikovAleksandr-IS <115283895+MelnikovAleksandr-IS@users.noreply.github.com>
2023-05-20 18:33:39 +0300

    Update shvijenerssklpotxt.py

--- lab_3/shvijenerssklpotxt.py
+++ lab_3/shvijenerssklpotxt.py
@@ -13,17 +13,5 @@ def shvijenerssklpotxt(message, key):
             cip += elm
     return cip       
   
-def decshvijenerssklpotxt(cip, key):
-    ormessage = ''
-    for i in range(len(cip)):
-        elm = cip[i]
-        if elm.isalpha():
-            sdvig = ord(key[i]) - ord('A')
-            ormessage += chr((ord(elm) - ord('A') - sdvig) % 26 + ord('A'))
-            key += cip[i]
-        else:
-            ormessage += elm
-    return ormessage
-  
 cip = shvijenerssklpotxt(message1, key1)
 print(cip)
