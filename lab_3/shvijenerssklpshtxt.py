fa99b409e4368a2abbc8982535ab3134f9fee204
MelnikovAleksandr-IS <115283895+MelnikovAleksandr-IS@users.noreply.github.com>
2023-06-03 12:41:36 +0300

    Update shvijenerssklpshtxt.py

--- lab_3/shvijenerssklpshtxt.py
+++ lab_3/shvijenerssklpshtxt.py
@@ -1,13 +1,19 @@
+message1 = 'WHYDOYOUWANT'
+key1 = 'A'
+
+def shvijenerssklpshtxt(message, key):
+    global key1
+    cip = ''
+    for i in range(len(message)):
+      cip += chr(((ord(message[i]) - 65) + (ord(key1[i]) - 65)) % 26 + 65)
+      key1 += cip[i]
+    return cip
+
 def decshvijenerssklpshtxt(cip, key):
-    ormessage = ''
-    for i in range(len(cip)):
-        elm = cip[i]
-        if elm.isalpha():
-            sdvig = ord(key[i]) - ord('A')
-            ormessage += chr((ord(elm) - ord('A') - sdvig) % 26 + ord('A'))
-            key += cip[i]
-        else:
-            ormessage += elm
-    return ormessage
-  
-  message = decshvijenerssklpshtxt
+  ormessage = ''
+  for i in range(len(cip)):
+    ormessage += chr(((ord(cip[i]) - 65) - (ord(key[i]) - 65)) % 26 +65)
+  return ormessage
+
+cip = shvijenerssklpshtxt(message1, key1)
+print(cip, decshvijenerssklpshtxt(cip, key1))
